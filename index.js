20201ad1690277e7dd00bafa1bb37083339797c3
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2024-02-03 15:39:27 +0400

    Create method the return gruoped ID

--- index.js
+++ index.js
@@ -1,61 +1,75 @@
 // 1
 class Singleton {
- static  #instance;
- 
- static getInstance() {
-    if(!this.#instance){
-        this.#instance = new Singleton();
+    static #instance;
+
+    static getInstance() {
+        if (!this.#instance) {
+            this.#instance = new Singleton();
+        }
+        return this.#instance;
+
     }
-    return this.#instance;
-    
- }
 
-async #fectData(apiUrl){
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data;
-}
+    async #fectData(apiUrl) {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    }
 
-async getData(apiUrl) {
-    return this.#fectData(apiUrl);
-}
+    async getData(apiUrl) {
+        return this.#fectData(apiUrl);
+    }
 
-gropuDataByPostId(){
-    const data = this.#fectData();
-    
-    const postId =  
+    ///3
+    gropDataByPostId() {
+        const data = this.getData('https://jsonplaceholder.typicode.com/posts');
 
-    const grupData = data.
-}
+        const grupData = data.reduce((acc, item) => {
 
+            const posId = item.id;
+            if (!acc[posId]) {
+                acc[posId] = [];
+            }
+            acc[posId].push(item);
+            return acc;
+        }, {});
+
+        return grupData;
+    }
+
+    getGroupData() {
+        return this.gropDataByPostId();
+    }
 }
 
 const data = Singleton.getInstance();
 data.getData('https://jsonplaceholder.typicode.com/posts')
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log(error.message);
-  });
+    .then(data => {
+        console.log(data);
+        console.log(data.gropDataByPostId());
+    })
+    .catch(error => {
+        console.log(error.message);
+    });
 
 
-  /// 2 
+/// 2 
 
-  Array.prototype.groupBy = function(val){
-    return this.reduce(function(acc , obj) {
+Array.prototype.groupBy = function (val) {
+    return this.reduce(function (acc, obj) {
         const key = obj[val];
 
-        if(!acc[key]){
+        if (!acc[key]) {
             acc[key] = [];
         }
 
         acc[key].push(obj);
         return acc;
-    },{}
+    }, {}
     )
-  }
+}
 
-  /// 3
+
+
 
 
